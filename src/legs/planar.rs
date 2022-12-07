30fa362ab68b4f32d7b1e1c7ee9f23a45ea40f96
Kuba Marek <blue.cube@seznam.cz>
2022-12-07 15:33:08 +0100

    leg planar kinematics: Fixed triangle point calculation

--- src/legs/planar.rs
+++ src/legs/planar.rs
@@ -52,7 +52,7 @@ fn perpendicular(v: Vector2<f64>) -> Vector2<f64> {
 /// Given two points `point_a` and `point_b` and two trigangle side lengths
 /// `length_a` (= |AC|) and `length_b` (= |BC|), calculates (one possible) point C.
 /// If no such triangle exists, returns None.
-fn find_triangle_point(point_a: Point2<f64>, length_a: f64, point_b: Point2<f64>, length_b: f64) -> Option<Point2<f64>> {
+fn find_triangle_point(point_a: &Point2<f64>, length_a: f64, point_b: &Point2<f64>, length_b: f64) -> Option<Point2<f64>> {
     // TODO: There are two solutions mirrored around AB, for now we just take one to the left from AB
     // TODO: Maybe also return an angle between X axis and AC.
 
@@ -67,16 +67,18 @@ fn find_triangle_point(point_a: Point2<f64>, length_a: f64, point_b: Point2<f64>
     let ba = point_b - point_a;
     let ba_norm_squared = ba.norm_squared();
 
-    let t = (length_a * length_a - length_b * length_b) / (2.0 * ba_norm_squared) + 0.5;
-    let s_squared = (length_a * length_a) / ba_norm_squared - t * t;
+    let p = (length_a * length_a) / ba_norm_squared;
+    let q = (length_b * length_b) / ba_norm_squared;
+
+    let t = (p - q + 1.0) / 2.0;
+    let s_squared = p - t * t;
     if s_squared < 0.0 {
-        return None
+        None
+    } else {
+        let s = s_squared.sqrt();
+        let point_c = point_a + ba * t + perpendicular(ba) * s;
+        Some(point_c)
     }
-    let s = s_squared.sqrt();
-
-    let point_c = point_a + ba * t + perpendicular(ba) * s;
-
-    Some(point_c)
 }
 
 fn calculate_state(point_f: Point2<f64>, params: &Params) -> Option<KinematicState> {
@@ -109,9 +111,23 @@ mod tests {
     use super::*;
     use proptest::prelude::*;
     use test_strategy::proptest;
+    use test_case::test_case;
     use nalgebra::proptest::vector;
-    use nalgebra::Const;
+    use nalgebra::{Const, distance_squared};
     use approx::assert_relative_eq;
+
+    macro_rules! assert_points_approx_eq {
+        ($p1:expr, $p2:expr, $max_dist:expr) => {
+            assert!(distance($p1, $p2) <= $max_dist,
+            "assert_points_approx_eq!({}, {}, {})
+
+    left  = {:?}
+    right = {:?}
+    distance(left, right) = {} > {}
+
+", stringify!($p1), stringify!($p2), stringify!($dist), $p1, $p2, distance($p2, $p1), $max_dist)
+        }
+    }
 
     fn angle_and_length_to_vector(angle: f64, length: f64) -> Vector2<f64> {
         Vector2::new(angle.cos(), angle.sin()) * length
@@ -186,36 +202,73 @@ mod tests {
         assert_eq!(v.dot(&perpendicular(v)), 0.0);
     }
 
+    #[test_case(Point2::new(0.0, 0.0), 5.0, Point2::new(3.0, 0.0), 4.0, Some(Point2::new(3.0, -4.0)); "pythagorean_triangle")]
+    #[test_case(Point2::new(0.0, 0.0), 1.0, Point2::new(3.0, 0.0), 1.0, None; "too_short")]
+    #[test_case(Point2::new(0.0, 0.0), 10.0, Point2::new(3.0, 0.0), 1.0, None; "too_long")]
+    fn find_triangle_point_examples(point_a: Point2<f64>, length_a: f64, point_b: Point2<f64>, length_b: f64, expected: Option<Point2<f64>>) {
+        let result = find_triangle_point(&point_a, length_a, &point_b, length_b);
+
+        match (result, expected) {
+            (Some(result), Some(expected)) => assert_points_approx_eq!(&result, &expected, 1e-6),
+            _ => assert_eq!(result, expected),
+        }
+    }
+
     #[proptest]
-    fn find_triangle_point_distances(
+    fn find_triangle_point_valid(
         #[strategy(point2_strategy(-1000.0, 1000.0))]
         point_a: Point2<f64>,
-        //#[strategy(proptest::num::f64::POSITIVE)]
-        #[strategy(0.0..1000.0)]
-        length_a: f64,
         #[strategy(point2_strategy(-1000.0, 1000.0))]
         point_b: Point2<f64>,
-        //#[strategy(proptest::num::f64::POSITIVE)]
-        #[strategy(0.0..1000.0)]
-        length_b: f64
+        #[strategy(point2_strategy(-1000.0, 1000.0))]
+        point_c: Point2<f64>,
     ) {
-        match find_triangle_point(point_a, length_a, point_b, length_b) {
-            None => {
-                let length_c = distance(&point_a, &point_b);
+        let length_a = distance(&point_a, &point_c);
+        let length_b = distance(&point_b, &point_c);
 
-                assert!(
-                    (length_a + length_b < length_c) ||
-                    (length_a + length_c < length_b) ||
-                    (length_b + length_c < length_a),
-                    "length_a = {}, length_b = {}, length_c = {}",
-                    length_a, length_b, length_c
-                );
-            }
-            Some(point_c) => {
-                assert_relative_eq!(distance(&point_a, &point_c), length_a, max_relative=1e-6);
-                assert_relative_eq!(distance(&point_b, &point_c), length_b, max_relative=1e-6);
-            }
-        }
+        let result = find_triangle_point(&point_a, length_a, &point_b, length_b).unwrap();
+
+        assert_relative_eq!(distance(&point_a, &result), length_a, max_relative=1e-6);
+        assert_relative_eq!(distance(&point_b, &result), length_b, max_relative=1e-6);
+
+        // assert_points_approx_eq!(point_c, result, 1e-6);
+            // Does not work 50% of time because of the "arbitrary" choice of one of the solutions
+    }
+
+    #[proptest]
+    fn find_triangle_point_too_short(
+        #[strategy(point2_strategy(-1000.0, 1000.0))]
+        point_a: Point2<f64>,
+        #[strategy(point2_strategy(-1000.0, 1000.0))]
+        point_b: Point2<f64>,
+        #[strategy(0.0..1.0)]
+        ab_fraction: f64,
+        #[strategy(0.0..1.0)]
+        a_fraction: f64
+    ) {
+        let length_ab = distance(&point_a, &point_b) * ab_fraction;
+        let length_a = length_ab * a_fraction;
+        let length_b = length_ab - length_a;
+
+        assert_eq!(find_triangle_point(&point_a, length_a, &point_b, length_b), None);
+    }
+
+    #[proptest]
+    fn find_triangle_point_too_long(
+        #[strategy(point2_strategy(-1000.0, 1000.0))]
+        point_a: Point2<f64>,
+        #[strategy(point2_strategy(-1000.0, 1000.0))]
+        point_b: Point2<f64>,
+        #[strategy(1.0..100.0)]
+        a_fraction: f64,
+        #[strategy(0.0..1.0)]
+        b_fraction: f64
+    ) {
+        let d = distance(&point_a, &point_b);
+        let length_a = d * a_fraction;
+        let length_b = (length_a - d) * b_fraction;
+
+        assert_eq!(find_triangle_point(&point_a, length_a, &point_b, length_b), None);
     }
 
     //#[test]
